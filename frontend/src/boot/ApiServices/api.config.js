f374ae12bf0542aee8bc75b9ba355528f0a6bdd2
Angel Isaac Fonseca Gómez <Magnua321@gmail.com>
2025-02-12 09:04:57 -0600

    last changes

--- frontend/src/boot/ApiServices/api.config.js
+++ frontend/src/boot/ApiServices/api.config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const url = 'http://localhost:3000/';
-// const url = 'https://l0rw7346-3000.usw3.devtunnels.ms/';
+// const url = 'http://localhost:3000/';
+const url = 'http://47.236.145.45:3000/';
 const api = axios.create({
     baseURL: `${url}api/` 
 });
